3c9fd3c60237e16d22d7844414b4146d9a2f354c
Kevin Ruggiero <kevinbruggiero@gmail.com>
2019-02-03 04:40:04 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -61,7 +61,7 @@ end
 
 def shortened_tweet_truncator(tweet)
   if word_substituter(tweet).chars.length > 140
-    tru = word_substituter(tweet).chars[0..136].push("...").join("")
+    word_substituter(tweet).chars[0..136].push("...").join("")
   else
     word_substituter(tweet)
   end
